a875e3efd1e0db7055da260c55c796f466d44e9d
boluwatife19 <aregbesolapaul19@gmail.com>
2024-07-13 11:55:52 +0100

    first commit

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -49,14 +49,14 @@ let contact =  "#contact";
 
 export function ButtonThree({children }) {
   return (
-        <Link
+        <div
           className="relative px-5 py-2 font-medium text-black group"
         >
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-slate-500 group-hover:bg-slate-700 group-hover:skew-x-12"></span>
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-white group-hover:bg-slate-300 group-hover:-skew-x-12"></span>
           <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-slate-600 -rotate-12"></span>
           <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-slate-400 -rotate-12"></span>
-          <span className="relative">{children}</span>
-        </Link>
+          <span className="relative">{children}!</span>
+        </div>
   );
 }
