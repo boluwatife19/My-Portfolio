aaf8680352f0e38d47d131a60f874107b1cdb65a
boluwatife19 <aregbesolapaul19@gmail.com>
2024-07-13 13:43:22 +0100

    first commit

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -56,7 +56,7 @@ export function ButtonThree({children }) {
           <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-white group-hover:bg-slate-300 group-hover:-skew-x-12"></span>
           <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-slate-600 -rotate-12"></span>
           <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-slate-400 -rotate-12"></span>
-          <span className="relative">{children}!</span>
+          <span className="relative">{children}</span>
         </div>
   );
 }
